100829180c682346fd30a681a09b09f26584df95
Ttou <1361572192@qq.com>
2024-11-01 07:27:55 +0000

    完善

--- src/configuration.ts
+++ src/configuration.ts
@@ -3,4 +3,8 @@ import { join } from 'node:path'
 
 import { parse } from 'yaml'
 
-export default () => parse(readFileSync(join(process.cwd(), 'config.yaml'), 'utf-8'))
+function configFile() {
+  return process.env.NODE_ENV ? `config.${process.env.NODE_ENV}.yaml` : 'config.yaml'
+}
+
+export default () => parse(readFileSync(join(process.cwd(), configFile()), 'utf-8'))
